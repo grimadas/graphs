9ba672fd767e66bd5ca98e95ce9f248d04d90db5
grimadas <bulat_nab@mail.ru>
2015-10-04 19:12:56 +0300

    Quelle 1

--- graphs.cu
+++ graphs.cu
@@ -3,24 +3,39 @@
 	Author : Bulat, 2015
 
 */
-#include "graph.h"
+#include "graph.cuh"
+#include "apsp.cuh"
+
 
 
 int main()
 {
+
 	Graph graph;
 	graph.init_test_graph(); // Reading graph from the file in COO format
 	graph.print_coo_graph();
 	graph.convert_to_CSR();
-	graph.print_csr_graph();	
+//	graph.print_csr_graph();
 
-	// BFS 
+	UINT wTimerRes = 0;
+	bool init = InitMMTimer(wTimerRes);
+	DWORD startTime = timeGetTime();
+
+	//test_funct<<<1, 1>>>(thrust::raw_pointer_cast(graph.full_vertex_array.data()), 4);
+
+	// BFS
 	//graph.single_bfs(2);
-	graph.form_full_level_graph();
-	//graph.print_csr_graph();
-	graph.calc_L_opacity();
-	graph.print_opacity_matrix();
+//	graph.form_full_level_graph();
+//	graph.print_csr_graph();
+//	graph.calc_L_opacity();
+//	graph.print_opacity_matrix();
 
-	
+	unsigned int endTime = timeGetTime();
+	unsigned int gpu_time = unsigned int(endTime - startTime);
+	printf("GPU Timing(including all device-host, host-device copies, device allocations and freeing of device memory): %dms\n\n", gpu_time);
+	DestroyMMTimer(wTimerRes, init);
+
+
+
 	return 0;
 }
